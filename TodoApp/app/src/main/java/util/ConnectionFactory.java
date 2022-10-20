f2e256537350451d32f5bcd6f81b45b1a5afa315
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-10-20 13:52:01 -0300

    Add 3 different methods to close a connection

--- TodoApp/app/src/main/java/util/ConnectionFactory.java
+++ TodoApp/app/src/main/java/util/ConnectionFactory.java
@@ -6,6 +6,8 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -32,6 +34,40 @@ public class ConnectionFactory {
         try {
             if (connection != null){
                 connection.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o "
+                    + "banco de dados");
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement){
+        try {
+            if (connection != null){
+                connection.close();
+            }
+            
+            if(statement != null){
+                statement.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o "
+                    + "banco de dados");
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet){
+        try {
+            if (connection != null){
+                connection.close();
+            }
+            
+            if(statement != null){
+                statement.close();
+            }
+            
+            if (resultSet != null){
+                resultSet.close();
             }
         } catch (Exception e) {
             throw new RuntimeException("Erro ao fechar a conexão com o "
