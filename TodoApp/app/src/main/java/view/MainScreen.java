6ce0a276a65d0c784ae2e262857ea4712940ce42
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-10-27 10:32:50 -0300

    Completed task save to database

--- TodoApp/app/src/main/java/view/MainScreen.java
+++ TodoApp/app/src/main/java/view/MainScreen.java
@@ -275,6 +275,11 @@ public class MainScreen extends javax.swing.JFrame {
         jTableTasks.setSelectionBackground(new java.awt.Color(153, 0, 153));
         jTableTasks.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jTableTasks.setShowHorizontalLines(true);
+        jTableTasks.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableTasksMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTableTasks);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
@@ -344,6 +349,24 @@ public class MainScreen extends javax.swing.JFrame {
         taskDialogScreen.setVisible(true);
         
     }//GEN-LAST:event_jLabelTasksAddMouseClicked
+
+    private void jTableTasksMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableTasksMouseClicked
+        // TODO add your handling code here:
+        
+        int rowIndex = jTableTasks.rowAtPoint(evt.getPoint());
+        int columnIndex = jTableTasks.columnAtPoint(evt.getPoint());
+        
+        switch(columnIndex){
+
+            case 3 -> {
+                Task task = taskModel.getTasks().get(rowIndex);
+                taskController.update(task);
+            }
+            case 4 -> {
+            }
+           
+        }
+    }//GEN-LAST:event_jTableTasksMouseClicked
 
     /**
      * @param args the command line arguments
